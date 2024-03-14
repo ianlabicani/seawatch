b5321f17f3c725037dfbf55e78726b8e7c563a4b
Ian Labicani <labicaniian@gmail.com>
2024-03-14 16:35:31 +0800

    Update Firebase configuration in environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    projectId: 'capstone-63f36',
-    appId: '1:969073261181:web:7bed6e8dd209597fafcf14',
-    storageBucket: 'capstone-63f36.appspot.com',
     apiKey: 'AIzaSyDqN-da0lcsO7G90djx0rTrqCihuQOrheg',
     authDomain: 'capstone-63f36.firebaseapp.com',
+    projectId: 'capstone-63f36',
+    storageBucket: 'capstone-63f36.appspot.com',
     messagingSenderId: '969073261181',
+    appId: '1:969073261181:web:7bed6e8dd209597fafcf14',
   },
   useEmulators: false,
 };
