3997a93196a3839e17df3f8c2297a0662eeb75ee
Ian Labicani <labicaniian@gmail.com>
2024-03-14 15:56:05 +0800

    Add user display name and logout functionality to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Auth, user, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  afauth = inject(Auth);
+  router = inject(Router);
+  userSignal = toSignal(user(this.afauth));
 
+  constructor() {}
+
+  ngOnInit() {}
+
+  signOut() {
+    from(signOut(this.afauth)).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+    });
+  }
 }
