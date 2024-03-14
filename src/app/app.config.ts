15dcfbd9ae720885082d1eb7a9960a34e5e0c73a
Ian Labicani <labicaniian@gmail.com>
2024-03-14 15:45:39 +0800

    Add Firebase configuration to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,41 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    ),
+    importProvidersFrom(
+      provideAuth(() => {
+        const auth = getAuth();
+        if (environment.useEmulators) {
+          connectAuthEmulator(auth, 'http://localhost:9099', {
+            disableWarnings: true,
+          });
+        }
+        return auth;
+      })
+    ),
+    importProvidersFrom(
+      provideFirestore(() => {
+        const firestore = getFirestore();
+        if (environment.useEmulators) {
+          connectFirestoreEmulator(firestore, 'localhost', 8080);
+        }
+        return firestore;
+      })
+    ),
+  ],
 };
