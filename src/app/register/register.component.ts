dadcc6526fac956dc49e01440c75b058d578b980
Ian Labicani <labicaniian@gmail.com>
2024-03-14 15:45:48 +0800

    Refactor register component form and add password confirmation validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from '@angular/fire/auth';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-register',
@@ -10,26 +18,88 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  private fb = inject(FormBuilder);
+  fb = inject(FormBuilder);
+  destroyRef = inject(DestroyRef);
+  router = inject(Router);
+  afauth = inject(Auth);
 
   registerForm = this.fb.nonNullable.group({
-    username: ['', [Validators.required]],
+    username: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
   });
   validationMessages = {
-    username: [{ type: 'required', message: 'Username is required' }],
+    username: [
+      { type: 'required', message: 'Email is required' },
+      {
+        type: 'minlength',
+        message: 'Username must be at least 3 characters long',
+      },
+    ],
     email: [
       { type: 'required', message: 'Email is required' },
-      { type: 'email', message: 'Invalid email format' },
+      { type: 'email', message: 'Email must be a valid email address' },
     ],
     password: [
       { type: 'required', message: 'Password is required' },
-      { type: 'minlength', message: 'Password must be at least 6 characters' },
+      {
+        type: 'minlength',
+        message: 'Password must be at least 6 characters long',
+      },
+    ],
+    confirmPassword: [
+      { type: 'required', message: 'Confirm Password is required.' },
+      { type: 'passwordsMatch', message: "Password doesn't match." },
     ],
   };
+  errorMessage: string | null = null;
 
-  onSubmit() {
-    throw new Error('Not implemented');
+  get passwordsMatch() {
+    return (
+      this.registerForm.get('password')?.value ===
+      this.registerForm.get('confirmPassword')?.value
+    );
+  }
+
+  constructor() {}
+
+  ngOnInit() {
+    this.registerForm
+      .get('confirmPassword')
+      ?.valueChanges.pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((val) => {
+        if (this.registerForm.get('confirmPassword')?.hasError('required'))
+          return;
+        if (!this.passwordsMatch) {
+          this.registerForm
+            .get('confirmPassword')
+            ?.setErrors({ passwordsMatch: true });
+        }
+      });
+  }
+
+  register() {
+    const { username, email, password } = this.registerForm.getRawValue();
+    from(
+      createUserWithEmailAndPassword(this.afauth, email, password)
+    ).subscribe({
+      next: (userCredential) => {
+        from(
+          updateProfile(userCredential.user, {
+            displayName: username,
+          })
+        ).subscribe({
+          next: () => {
+            this.router.navigate(['/home']);
+          },
+        });
+      },
+      error: (err) => {
+        console.log(err.message);
+
+        this.errorMessage = err.message;
+      },
+    });
   }
 }
