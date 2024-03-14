400469fe304e042e29696e925480468ddc4182a7
Ian Labicani <labicaniian@gmail.com>
2024-03-14 15:44:37 +0800

    Add development and production environment configurations

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: 'capstone-63f36',
+    appId: '1:969073261181:web:7bed6e8dd209597fafcf14',
+    storageBucket: 'capstone-63f36.appspot.com',
+    apiKey: 'AIzaSyDqN-da0lcsO7G90djx0rTrqCihuQOrheg',
+    authDomain: 'capstone-63f36.firebaseapp.com',
+    messagingSenderId: '969073261181',
+  },
+  useEmulators: false,
+};
