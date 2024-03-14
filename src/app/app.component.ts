ca07db0b85afb19f4837e202886eef84a0b50b8b
Ian Labicani <labicaniian@gmail.com>
2024-03-14 21:25:45 +0800

    Add environment logging in AppComponent constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'seawatch';
+  constructor() {
+    console.log(environment.useEmulators);
+  }
 }
