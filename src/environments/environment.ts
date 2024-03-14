0110161242bc8f3b420a3289ec965beb61b48753
Ian Labicani <labicaniian@gmail.com>
2024-03-14 17:01:18 +0800

    Update environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyDqN-da0lcsO7G90djx0rTrqCihuQOrheg',
     authDomain: 'capstone-63f36.firebaseapp.com',
@@ -8,5 +8,5 @@ export const environment = {
     messagingSenderId: '969073261181',
     appId: '1:969073261181:web:7bed6e8dd209597fafcf14',
   },
-  useEmulators: false,
+  useEmulators: true,
 };
